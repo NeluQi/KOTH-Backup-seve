096384b8e29ec6a39e20f6eed271e9fd59bc39ea
Egor <egor2998067@gmail.com>
2016-09-06 20:02:50 +0300

    00
    00

---
+++ Sourse (Исходник на Visual Studio 2015)/backup KOTH/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace backup_KOTH
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.менюToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.инфоToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.time = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.comment = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.unsave = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.dell = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.dir = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.time,
+            this.comment,
+            this.unsave,
+            this.dell,
+            this.dir});
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.ActiveBorder;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 27);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(436, 208);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellEndEdit);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.менюToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(436, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // менюToolStripMenuItem
+            // 
+            this.менюToolStripMenuItem.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.менюToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.инфоToolStripMenuItem});
+            this.менюToolStripMenuItem.Name = "менюToolStripMenuItem";
+            this.менюToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.менюToolStripMenuItem.Text = "Меню";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(12, 240);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 36);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Сделать Backup";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(257, 240);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(167, 36);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Восстановить последний";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // инфоToolStripMenuItem
+            // 
+            this.инфоToolStripMenuItem.Name = "инфоToolStripMenuItem";
+            this.инфоToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.инфоToolStripMenuItem.Text = "Информация";
+            this.инфоToolStripMenuItem.Click += new System.EventHandler(this.инфоToolStripMenuItem_Click);
+            // 
+            // time
+            // 
+            this.time.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            dataGridViewCellStyle1.Format = "f";
+            dataGridViewCellStyle1.NullValue = null;
+            this.time.DefaultCellStyle = dataGridViewCellStyle1;
+            this.time.HeaderText = "Время";
+            this.time.Name = "time";
+            this.time.ReadOnly = true;
+            // 
+            // comment
+            // 
+            this.comment.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.comment.FillWeight = 150F;
+            this.comment.HeaderText = "Коментарии";
+            this.comment.Name = "comment";
+            // 
+            // unsave
+            // 
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.Green;
+            this.unsave.DefaultCellStyle = dataGridViewCellStyle2;
+            this.unsave.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.unsave.HeaderText = "Восстановить";
+            this.unsave.Name = "unsave";
+            this.unsave.ReadOnly = true;
+            this.unsave.Text = "Восстановить";
+            this.unsave.ToolTipText = "Восстановить";
+            // 
+            // dell
+            // 
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.dell.DefaultCellStyle = dataGridViewCellStyle3;
+            this.dell.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.dell.HeaderText = "Удалить";
+            this.dell.Name = "dell";
+            this.dell.ReadOnly = true;
+            this.dell.Text = "Удалить";
+            this.dell.ToolTipText = "Удалить";
+            // 
+            // dir
+            // 
+            this.dir.HeaderText = "Dir";
+            this.dir.Name = "dir";
+            this.dir.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(436, 289);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Backup Save KOTH | PreHack.ru";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem менюToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ToolStripMenuItem инфоToolStripMenuItem;
+        private System.Windows.Forms.DataGridViewTextBoxColumn time;
+        private System.Windows.Forms.DataGridViewTextBoxColumn comment;
+        private System.Windows.Forms.DataGridViewButtonColumn unsave;
+        private System.Windows.Forms.DataGridViewButtonColumn dell;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dir;
+    }
+}
+
---
+++ Sourse (Исходник на Visual Studio 2015)/backup KOTH/Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace backup_KOTH
+{
+    public partial class Form1 : Form
+    {
+        public static DateTime Today { get; }
+        public Form1()
+        {
+            InitializeComponent();
+            loadTo(dataGridView1);
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string saveDir = System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\Arma 3\";
+            DateTime thisDay = DateTime.Today;
+            DateTime thisNow = DateTime.Now;
+            string backupDir = Directory.GetCurrentDirectory() + "\\Backup\\" + thisDay.ToString("d") + " - " + thisNow.ToString("HH;mm;ss");
+            string DataNow = thisNow.ToString("HH:mm") + " - " + thisDay.ToString("d");
+   
+            try
+            {
+                string[] Arma3List = Directory.GetFiles(saveDir, "*.Arma3Profile");
+
+                foreach (string f in Arma3List)
+                {
+                    // Создание бэкапа
+                    string fName = f.Substring(saveDir.Length);
+                    Directory.CreateDirectory(backupDir);
+                    File.Copy(Path.Combine(saveDir, fName), Path.Combine(backupDir, fName), true);
+                }
+
+
+       
+            dataGridView1.Rows.Add(DataNow, "", "Восстановить", "Удалить", backupDir);
+            saveTo(dataGridView1);
+            }
+
+            catch (DirectoryNotFoundException dirNotFound)
+            {
+                Console.WriteLine(dirNotFound.Message);
+                MessageBox.Show("Ошибка");
+            }
+
+
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+
+                // Восстонов.
+                if (e.ColumnIndex == 2)
+                {
+
+
+                    string saveDir = dataGridView1.Rows[e.RowIndex].Cells[4].Value.ToString();
+                    string backupDir = System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\Arma 3\";
+
+
+                    string[] Arma3List = Directory.GetFiles(saveDir, "*.Arma3Profile");
+
+                    foreach (string f in Arma3List)
+                    {
+                        string fName = f.Substring(saveDir.Length + 1);
+                        File.Copy(Path.Combine(saveDir, fName), Path.Combine(backupDir, fName), true);
+                    }
+                    MessageBox.Show("Успешно!");
+
+                }
+
+                // Удаление
+                if (e.ColumnIndex == 3)
+                {
+                    string saveDir = dataGridView1.Rows[e.RowIndex].Cells[4].Value.ToString();
+                    dataGridView1.Rows.RemoveAt(e.RowIndex);
+                    string[] Arma3List = Directory.GetFiles(saveDir, "*.Arma3Profile");
+
+                    for (int i = 0; i < Arma3List.Length; i++)
+                    {
+                        File.Delete(Arma3List[i]);
+                    }
+                    Directory.Delete(saveDir);
+                }
+
+                saveTo(dataGridView1);
+
+            }
+
+            catch {}
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try{ 
+            int last = dataGridView1.Rows.Count - 1;
+            string saveDir = dataGridView1.Rows[last].Cells[4].Value.ToString();
+            string backupDir = System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\Arma 3\";
+
+
+            string[] Arma3List = Directory.GetFiles(saveDir, "*.Arma3Profile");
+
+            foreach (string f in Arma3List)
+            {
+                string fName = f.Substring(saveDir.Length + 1);
+                File.Copy(Path.Combine(saveDir, fName), Path.Combine(backupDir, fName), true);
+            }
+            saveTo(dataGridView1);
+            MessageBox.Show("Успешно!");
+
+        }
+
+            catch { MessageBox.Show("Ошибка"); }
+        }
+
+        public static void saveTo(DataGridView dr)
+        {
+            try
+            {
+                //DataGridView dgv=...
+                string file = "data.bin";
+                using (BinaryWriter bw = new BinaryWriter(File.Open(file, FileMode.Create)))
+                {
+                    bw.Write(dr.Columns.Count);
+                    bw.Write(dr.Rows.Count);
+                    foreach (DataGridViewRow dgvR in dr.Rows)
+                    {
+                        for (int j = 0; j < dr.Columns.Count; ++j)
+                        {
+                            object val = dgvR.Cells[j].Value;
+                            if (val == null)
+                            {
+                                bw.Write(false);
+                                bw.Write(false);
+                            }
+                            else
+                            {
+                                bw.Write(true);
+                                bw.Write(val.ToString());
+                            }
+                        }
+                    }
+                }
+            }
+            catch { MessageBox.Show("Ошибка сохранения"); }
+        }
+
+        public static void loadTo(DataGridView dr)
+        {
+            try { 
+
+            dr.Rows.Clear();
+            string file = "data.bin";
+            using (BinaryReader bw = new BinaryReader(File.Open(file, FileMode.Open)))
+            {
+                int n = bw.ReadInt32();
+                int m = bw.ReadInt32();
+                for (int i = 0; i < m; ++i)
+                {
+                    dr.Rows.Add();
+                    for (int j = 0; j < n; ++j)
+                    {
+                        if (bw.ReadBoolean())
+                        {
+                            dr.Rows[i].Cells[j].Value = bw.ReadString();
+                        }
+                        else bw.ReadBoolean();
+                    }
+                }
+            }
+
+        }
+
+            catch
+            {
+
+            }
+
+
+        }
+        private void dataGridView1_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            saveTo(dataGridView1);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            saveTo(dataGridView1);
+        }
+
+        private void инфоToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Автор - https://vk.com/id208497682 \n PreHack.ru");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
